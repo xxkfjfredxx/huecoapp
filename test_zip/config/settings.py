34f8eb5a41da322fd6a5845b6e576a88e62db278
Fred Rueda <fredruedao@gmail.com>
2026-03-11 18:15:02 -0500

    chore: remove redundant test_zip folder and clean up gitignore

--- test_zip/config/settings.py
+++
@@ -1,4 +0,0 @@
-# settings.py
-# Configuración de Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
--- test_zip/config/settings/development.py
+++
@@ -1,5 +0,0 @@
-from .base import *  # noqa: F403,F401
-
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
